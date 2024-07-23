e9a5ba7b010a07380db25d95bd8b7fd2e8068963
WinterEffect <yishaigame999@gmail.com>
2024-07-23 13:47:11 +0300

    added install script

--- setup.sh
+++ setup.sh
@@ -1,34 +1,12 @@
 
 
 # installed packages (with pacman)
-# git
-# base-devel
-# fastfetch
-# neovim
-# swww
-# hyprpaper
-# hypridle
-# hyprlock
-# yazi
-# openssh
-# firefox
-# bat
-# eza
-# zoxide
-# btop
-# fzf
-# stow
-
-# devtools
-# gcc
-# rust
-# nodejs
-# python3
+pacman -S git base-devel fastfetch neovim swww hyprpaper hypridle hyprlock yazi openssh firefox bat eza zoxide btop fzf stow devtools gcc rust nodejs python3
 
 # yay setup
-# git clone https://aur.archlinux.org/yay.git
-# cd yay
-# makepkg -si
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
 #
 # installed packages (with yay)
-# aylurs-gtk-shell
+yay -S aylurs-gtk-shell
