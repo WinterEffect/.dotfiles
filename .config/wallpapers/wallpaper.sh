c90367ea90ceda661c5460b5c395c2d245dc30dc
WinterEffect <yishaigame999@gmail.com>
2024-07-23 13:43:55 +0300

    added new customization

---
+++ .config/wallpapers/wallpaper.sh
@@ -0,0 +1,15 @@
+DIR=$HOME/.config/wallpapers/ontime
+currenttime=$(date +%H:%M)
+if [[ "$currenttime" > "20:00" ]] && [[ "$currenttime" < "04:00" ]]; then
+    IMG=$DIR/night.webp
+elif [[ "$currenttime" > "04:00" ]] && [[ "$currenttime" < "11:00" ]]; then 
+    IMG=$DIR/morning.webp
+elif [[ "$currenttime" > "11:00" ]] && [[ "$currenttime" < "16:00" ]]; then
+    IMG=$DIR/noon.webp
+else
+    IMG=$DIR/afternoon.webp
+fi
+echo $IMG
+swww query || swww init
+
+swww img ${IMG} --transition-fps 30 --transition-type random --transition-duration 2
---
+++ setup.sh
@@ -0,0 +1,34 @@
+
+
+# installed packages (with pacman)
+# git
+# base-devel
+# fastfetch
+# neovim
+# swww
+# hyprpaper
+# hypridle
+# hyprlock
+# yazi
+# openssh
+# firefox
+# bat
+# eza
+# zoxide
+# btop
+# fzf
+# stow
+
+# devtools
+# gcc
+# rust
+# nodejs
+# python3
+
+# yay setup
+# git clone https://aur.archlinux.org/yay.git
+# cd yay
+# makepkg -si
+#
+# installed packages (with yay)
+# aylurs-gtk-shell
